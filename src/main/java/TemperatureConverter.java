d7490dd7309d3c27233b4ed7cea82159a1ef3089
Peetu <peetu.rasanen@metropolia.fi>
2024-09-27 12:34:32 +0300

    initial commit

---
+++ src/main/java/TemperatureConverter.java
@@ -0,0 +1,42 @@
+public class TemperatureConverter {
+
+    /**
+     * Converts Fahrenheit to Celsius.
+     *
+     * @param fahrenheit The temperature in Fahrenheit.
+     * @return The temperature in Celsius.
+     */
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    /**
+     * Converts Celsius to Fahrenheit.
+     *
+     * @param celsius The temperature in Celsius.
+     * @return The temperature in Fahrenheit.
+     */
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    /**
+     * Checks if the given Celsius temperature is extreme.
+     *
+     * @param celsius The temperature in Celsius.
+     * @return True if the temperature is below -40 or above 50, otherwise false.
+     */
+    public boolean isExtremeTemperature(double celsius) {
+        return celsius <= -40 || celsius >= 50;
+    }
+
+    /**
+     * Converts Kelvin to Fahrenheit.
+     *
+     * @param kelvin The temperature in Kelvin.
+     * @return The temperature in Fahrenheit.
+     */
+    public double kelvinToFahrenheit(double kelvin) {
+        return Math.round((kelvin - 273.15) * 9 / 5 + 32);
+    }
+}
